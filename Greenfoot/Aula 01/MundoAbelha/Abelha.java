8ea4bae23920a31ffb46662d3cc9dd046c21d956
Rick Alex <ricketttohs@gmail.com>
2025-06-09 21:53:02 -0300

    65001

--- Greenfoot/Aula 01/MundoAbelha/Abelha.java
+++ Greenfoot/Aula 01/MundoAbelha/Abelha.java
@@ -9,9 +9,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Abelha extends Actor
 {
     //Definindo os fields
-    int vidas;
-    int score;
-    int PONTOS = 100;
+    private int vidas;
+    private int score;
+    private int PONTOS = 100;
+    private GreenfootImage imgs[];
+    private int indice;
     //Definindo o constructor
     /**
      * Constructor da Classe Abelha
@@ -19,6 +21,13 @@ public class Abelha extends Actor
     public Abelha(){
         vidas = 3; //vai ter 3 vidas
         score = 0;
+        //GreenfootImage img = new GreenfootImage("bee01.png"); setImage(img);
+        indice= 0;
+        imgs = new GreenfootImage[4];//Definindo vetor 4 posições{
+        for (int i=0;i<4;i++){
+            imgs[i] = new GreenfootImage("bee0" + (i+1) + ".png");
+        }
+        setImage(imgs[indice]);
     }
 
     /**
@@ -38,6 +47,7 @@ public class Abelha extends Actor
         verificarPosicao();
         capturarMosca();
         serCapturadoPelaAranha();
+        animarAbelha();
     }
 
     /**
@@ -102,7 +112,7 @@ public class Abelha extends Actor
             int posY = Greenfoot.getRandomNumber(
                     getWorld().getHeight()) + 1;
             //Criando a mosca
-            Mosca mosca = new Mosca();
+            Mosca mosca = new Mosca(Greenfoot.getRandomNumber(3) + 1, Greenfoot.getRandomNumber(360));
             //Colocando no mundo na posição X, Y
             getWorld().addObject(mosca, posX, posY);
         }
@@ -136,6 +146,11 @@ public class Abelha extends Actor
     public void atualizarScore(){
         score += PONTOS;//score = score + PONTOS
         getWorld().showText("Score: " + score, 100, 10);
+    }
+    
+    public void animarAbelha(){
+        indice = (indice + 1) % 4;
+        setImage(imgs[indice]);
     }
 }
 
