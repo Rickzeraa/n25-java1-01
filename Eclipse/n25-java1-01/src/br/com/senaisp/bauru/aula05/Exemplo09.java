b421a8f3a837d8e0b7eef3edd94c4a0faee97f29
Rick Alex <ricketttohs@gmail.com>
2025-06-17 21:46:24 -0300

    65001

---
+++ Eclipse/n25-java1-01/src/br/com/senaisp/bauru/aula05/Exemplo09.java
@@ -0,0 +1,23 @@
+package br.com.senaisp.bauru.aula05;
+
+import java.util.Scanner;
+
+public class Exemplo09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Scanner sc = new Scanner(System.in);
+			//Ultizando uma estrutura while
+			String res="S";
+			while (!res.equals("s") && !res.equals("N")) {
+				System.out.println("Digite S ou N: ");
+				res = sc.nextLine();
+			}
+			System.out.println("Saiu!");
+			sc.close();
+		}
+
+	}
+
+
---
+++ Eclipse/n25-java1-01/src/br/com/senaisp/bauru/aula05/Exercicio01.java
@@ -0,0 +1,29 @@
+package br.com.senaisp.bauru.aula05;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Usando o scanner para ler um arquivo texto
+		Scanner sc = new Scanner(Exercicio01.class.getResourceAsStream("arquivo.txt"));
+			String linha = sc.nextLine(); //pegando cabeçalho
+			Scanner sca = new Scanner(linha);
+			sca.useDelimiter(";");
+			//Imprimindo o cabeçalho
+			while(sca.hasNext()) {
+				System.out.print(sca.next()+"\t\t");
+			}
+			System.out.println();//pulando a linha
+			sca.close();//fechando o scanner de apoio
+		while(sc.hasNextLine()) {
+			linha = sc.nextLine();
+			sca = new Scanner(linha);
+			sca.useDelimiter(";");
+			
+		}
+
+	}
+
+}
